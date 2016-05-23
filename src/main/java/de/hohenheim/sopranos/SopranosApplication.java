f0391b7917a2824833512327e14bb9a3b6b99932
mortmann <st111523@stud.uni-stuttgart.de>
2016-05-23 17:41:33 +0200

    Project

---
+++ src/main/java/de/hohenheim/sopranos/SopranosApplication.java
@@ -0,0 +1,12 @@
+package de.hohenheim.sopranos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SopranosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SopranosApplication.class, args);
+	}
+}
